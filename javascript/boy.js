b32a77213da1242be6161035e3ad37ece8e42151
victor <victor.tm90@gmail.com>
2022-02-09 11:15:00 +0100

    fuente

--- javascript/boy.js
+++ javascript/boy.js
@@ -2,7 +2,6 @@ class Boy {
   constructor() {
     this.x = canvas.width / 2; // posición en el eje x
     this.y = canvas.height / 2; // posicion en el eje y
-
     this.width = 90;
     this.height = 105;
     this.img = new Image();
@@ -34,10 +33,10 @@ class Boy {
   moveBoy = (event) => {
     if (event.key === "ArrowRight") {
       this.x = this.x + 50;
-      this.img.src = "../img/run-boy.png"
+      this.img.src = "./img/run-boy.png"
     } else if (event.key === "ArrowLeft") {
       this.x = this.x - 50;
-      this.img.src = "../img/run-boy-left.png"
+      this.img.src = "./img/run-boy-left.png"
     } else if (event.key === "ArrowUp") {
       this.y = this.y - 50;
     } else if (event.key === "ArrowDown") {
